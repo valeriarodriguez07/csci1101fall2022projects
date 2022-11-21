a1b105e6e82e3e12bda90b797ded4c859a023a84
valeria <valedelrodr07@gmail.com>
2022-11-21 14:31:52 -0600

    Add games data project

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '1992-01-02 00:00:00' AND '1992-12-31 23:59:59' ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (45165, 0, -1205164513.187412)
+
+--UPDATE sandbox SET in64_value = 21548 WHERE id = 25;
+
+--DELETE FROM sandbox WHERE id = 36;
